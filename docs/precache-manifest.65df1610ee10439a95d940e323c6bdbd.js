7f5a921066535f3aae71177608d8fad04ac228d7
admin <rootaka@gmail.com>
2020-01-29 10:31:04 +0900

    2nd commit

---
+++ docs/precache-manifest.65df1610ee10439a95d940e323c6bdbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e8c8280c74c589ce268c87f795fea9c",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "8daae09c20bdd0f92d9f",
+    "url": "/tic-tac-toe/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0b39366b205d4d1e8be7",
+    "url": "/tic-tac-toe/static/js/2.13641a68.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tic-tac-toe/static/js/2.13641a68.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8daae09c20bdd0f92d9f",
+    "url": "/tic-tac-toe/static/js/main.91b72497.chunk.js"
+  },
+  {
+    "revision": "fdbd6fae367d142c593b",
+    "url": "/tic-tac-toe/static/js/runtime-main.df4931a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic-tac-toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
